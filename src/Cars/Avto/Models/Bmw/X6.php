b430b88c5ab17eaca88de16dd41f7fd6f2e2db4d
romeoclis <robert.rollun@gmail.com>
2022-10-25 12:55:36 +0000

    cars library

---
+++ src/Cars/Avto/Models/Bmw/X6.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Robert\Cars\Avto\Models\Bmw;
+
+use Robert\Cars\Avto\Models\AbstractModel;
+
+class X6 extends AbstractModel
+{
+    public function getModel(): string
+    {
+        return 'X6';
+    }
+}
---
+++ src/Cars/Avto/Models/Bmw/X8.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Robert\Cars\Avto\Models\Bmw;
+
+use Robert\Cars\Avto\Models\AbstractModel;
+
+class X8 extends AbstractModel
+{
+    public function getModel(): string
+    {
+        return 'X8';
+    }
+}
